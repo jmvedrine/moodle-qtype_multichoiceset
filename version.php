9c7fa88ee0164d312f3ac273897629fa95d47d74
Eoin Campbell <campbeeo@tcd.ie>
2018-02-05 19:04:00 +0000

    Fix presentation of answers in Boost theme

--- version.php
+++ version.php
@@ -25,13 +25,13 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'qtype_multichoiceset';
-$plugin->version   = 2018013100;
+$plugin->version   = 2018020500;
 
 $plugin->requires  = 2015111600;
 
 $plugin->cron      = 0;
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '1.6 for Moodle 3.0 up to 3.5';
+$plugin->release   = '1.6.1 for Moodle 3.0 up to 3.5';
 
 $plugin->dependencies = array(
     'qtype_multichoice' => ANY_VERSION,
