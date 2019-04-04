f67db833266129657d30bb30999ceb98cab61755
Carlos Pasqualini <pasqualinic@fcal.uner.edu.ar>
2019-04-04 16:17:55 -0300

    Fix a few typos

--- lang/es/qtype_multichoiceset.php
+++ lang/es/qtype_multichoiceset.php
@@ -15,7 +15,7 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Strings for component 'qtype_multichoiceset', language 'en', branch 'MOODLE_20_STABLE'
+ * Strings for component 'qtype_multichoiceset', language 'es', branch 'MOODLE_20_STABLE'
  *
  * @package   qtype_multichoiceset
  * @copyright 1999 onwards Martin Dougiamas  {@link http://moodle.com}
